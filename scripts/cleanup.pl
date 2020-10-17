db85d4ab80b40da0e54193fae629a2eeb0612aaf
Ivan Savov <ivan.savov@gmail.com>
2020-10-16 22:33:38 -0400

    cleanup the cleanup script: Left only the essential pieces

--- scripts/cleanup.pl
+++ scripts/cleanup.pl
@@ -1,53 +1,27 @@
 #!/usr/bin/env perl -pi
-# multi-line in place substitute
 use strict;
 use warnings;
 
-BEGIN {undef $/;}     
+BEGIN {undef $/;}      # multi-line in-place substitute
 
-# noBSmath --> PolyTeX   substitutions
-# #################################################################### 
- 
-# TODO: automatically replace tables
-
-
-
+# minireference LaTeX --> Softcover PolyTeX substitutions
+################################################################################
 
 # remove enumitems customizations like  [topsep=0.4em,partopsep=0.4em] 
 s/\\begin\{enumerate\}\[.*\]/\\begin{enumerate}/g;
 s/\\begin\{itemize\}\[.*\]/\\begin{itemize}/g;
 
-# funky negative sign
+# simpify funky negative sign
 s/\\raisebox\{\.52ex\}\{\\rule\{0\.9em\}\{\.4pt\}\}/-/g;
 
-# change figures/ to images/figures/
+# change figures/ to images/figures/ (softcover expectes images dir for html build)
 s/\{figures/\{images\/figures/g;
 s/\{problems\/figures/\{images\/problems\/figures/g;
-
-# get rid of \intertext and \shortintertext
-#s/\\\\ *\n*([ \t]*)\\intertext\{([^}]*)\}/\n$1\\end{align\*}\n$1$2\n$1\\begin{align\*}/gs;
-#s/\\\\ *\n*([ \t]*)\\shortintertext\{([^}]*)\}/\n$1\\end{align\*}\n$1$2\n$1\\begin{align\*}/gs;
 
 # verb env for miniref code blocks
 s/\\begin\{verbatimtab\}/\\begin\{verbatim\}/g;
 s/\\end\{verbatimtab\}/\n\\end\{verbatim\}/g;
 
-# cleanup exercise sections that were oncatenated using cat
-#s/\\input\{problems\/chapter1_solving_systems_of_lin_eqns_exercises\.tex\}//g;
-#s/\\input\{problems\/chapter2_problems\.tex\}//g;
-#s/\\input\{problems\/chapter4_problems\.tex\}//g;
-#s/\\input\{problems\/chapter4_momentum_exercises\.tex\}//g;
-#s/\\input\{problems\/chapter4_energy_exercises\.tex\}//g;
-#s/\\input\{problems\/chapter5_problems\.tex\}//g;
-#s/\\input\{problems\/chapter5_limits_exercises\.tex\}//g;
-#s/\\input\{problems\/chapter5_volumes_of_revolution_exercises\.tex\}//g;
-
-# replace .pdf figures with .png
-s/\\includegraphics([^}]*).pdf\}/\\includegraphics$1.png\}/g;
-# remove options on \includegraphics
-# s/\\includegraphics\[[^\]]*\]\{/\\includegraphics\{/g;
-# TODO: preserve width props?
-#
 # handle ifthenelse marked up with special zBLOCK and ifEPUB syntax
 s/\n[^\n]*
 \{\{zBLOCK\}\}
@@ -60,17 +34,9 @@ s/\n[^\n]*
 
 # remove all comments
 s/([^\\])%.*?\n/$1/g;
-# change %= to % = because Polytexnic uses %= for pass-through comments...
-# s/\%=/% =/g;
-
-# add centering to wrapfigures
-# s/\\begin\{wrapfigure\}\{.*?\}\{.*?\}/\\begin\{figure\}[ht] \\centering/g;
-s/\\begin\{wrapfigure\}(\[.*?\])?(\{.*?\})+/\\begin\{figure\} \\centering/g;
-s/\\end\{wrapfigure\}/\\end\{figure\}/g;
 
 # Remove broken HREF to UAM align* block
 s/equation \\eqref\{UAM:v\}/velocity equation/g;
-
 
 # Appendix: Answers and Solutions
 s/\\showExerciseAnswers\{([^}]*)\}/\\input{99anssol\/eanswers_$1\}/g;
@@ -78,13 +44,11 @@ s/\\showExerciseSolutions\{([^}]*)\}/\\input{99anssol\/esolutions_$1\}/g;
 s/\\showProblemAnswers\{([^}]*)\}/\\input{99anssol\/answers_$1\}/g;
 s/\\showProblemSolutions\{([^}]*)\}/\\input{99anssol\/solutions_$1\}/g;
 
-
 # Calculus problems sections
 s/\\subsection\{Limits problems\}/\\large\{\\noindent\n\\textbf\{Limits problems\}\}/g;
 s/\\subsection\{Derivatives problems\}/\\large\{\\noindent\n\\textbf\{Derivatives problems\}\}/g;
 s/\\subsection\{Integrals problems\}/\\large\{\\noindent\n\\textbf\{Integrals problems\}\}/g;
 s/\\subsection\{Sequences and series problems\}/\\noindent\n\\large\{\\textbf\{Sequences and series problems\}\}/g;
-
 
 # Softsections to \section* in appendices
 s/\\softsection\{/\\section\*\{/g;
